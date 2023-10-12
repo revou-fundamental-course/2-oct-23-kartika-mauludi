0162f31f7f72951b8f3e3fe2ed457b056a112161
ludius01 <69234573+ludius01@users.noreply.github.com>
2023-10-12 19:09:52 +0700

    update final

--- js/javascript.js
+++ js/javascript.js
@@ -10,7 +10,7 @@ konvButton.addEventListener('click',function(){
     celcius = document.getElementById('celcius-input-field').value
     hasil = Number(celcius * (9/5) + 32);
     document.getElementById('fahrenheit-output-field').value = hasil
-    document.getElementById('kalkulasi').value = celcius + "C * (9/5) + 32 = " + hasil
+    document.getElementById('kalkulasi').value = celcius + "C * (9/5) + 32 = " + hasil + " F"
 })
 
 resetButton.addEventListener('click',function(){
@@ -23,7 +23,7 @@ reverseButton.addEventListener('click',function(){
     fahrenheit = document.getElementById('fahrenheit-output-field').value
     hasil = Number((fahrenheit - 32) * 5/9);
     document.getElementById('celcius-input-field').value = hasil
-    document.getElementById('kalkulasi').value = "( " + fahrenheit + "F - 32) * 5/9 = " + hasil
+    document.getElementById('kalkulasi').value = "( " + fahrenheit + "F - 32) * 5/9 = " + hasil + " C"
 })
 
 
