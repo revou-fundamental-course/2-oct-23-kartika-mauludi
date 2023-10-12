6edd65c19f03eed3635593486c9a881dd525ec48
ludius01 <69234573+ludius01@users.noreply.github.com>
2023-10-12 16:02:03 +0700

    update 12

--- js/javascript.js
+++ js/javascript.js
@@ -2,28 +2,28 @@ var celcius
 var fahrenheit
 var hasil
 
-const konvButton = document.getElementById('konversi')
-const resetButton = document.getElementById('reset')
-const reverseButton = document.getElementById('reverse') 
+const konvButton = document.getElementById('button-konversi')
+const resetButton = document.getElementById('button-reset')
+const reverseButton = document.getElementById('button-reverse') 
 
 konvButton.addEventListener('click',function(){
-    celcius = document.getElementById('celcius').value
+    celcius = document.getElementById('celcius-input-field').value
     hasil = Number(celcius * (9/5) + 32);
-    document.getElementById('farenheit').value = hasil
-    document.getElementById('kalkulasi').value = celcius + " * (9/5) + 32 = " + hasil
+    document.getElementById('fahrenheit-output-field').value = hasil
+    document.getElementById('kalkulasi').value = celcius + "C * (9/5) + 32 = " + hasil
 })
 
 resetButton.addEventListener('click',function(){
-    document.getElementById('farenheit').value = ""
-    document.getElementById('celcius').value = ""
+    document.getElementById('fahrenheit-output-field').value = ""
+    document.getElementById('celcius-input-field').value = ""
     document.getElementById('kalkulasi').value = ""  
 })
 
 reverseButton.addEventListener('click',function(){
-    fahrenheit = document.getElementById('farenheit').value
+    fahrenheit = document.getElementById('fahrenheit-output-field').value
     hasil = Number((fahrenheit - 32) * 5/9);
-    document.getElementById('celcius').value = hasil
-    document.getElementById('kalkulasi').value = "( " + fahrenheit + " - 32) * 5/9 = " + hasil
+    document.getElementById('celcius-input-field').value = hasil
+    document.getElementById('kalkulasi').value = "( " + fahrenheit + "F - 32) * 5/9 = " + hasil
 })
 
 
